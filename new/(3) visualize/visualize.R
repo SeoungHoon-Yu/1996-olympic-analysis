5d2fa794ad26ee894d7de4f4cf26595059f99aa1
seunghunii <rsh1502@naver.com>
2021-06-01 23:59:16 +0900

    20210601 folder arrange

--- new/(3) visualize/visualize.R
+++
@@ -1,8 +0,0 @@
-pkgs <- c('dplyr','ggplot2')
-sapply(pkgs, require, character.only = TRUE)
-
-load('D:/승훈/Data/Olympic analysis/my_new/(2) wrangle/wrangle_data.RData')
-
-# explore.
-glimpse(olym)
-
